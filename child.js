19f927e401e9358a7856c96ffdbd0b7a09308597
Todd Wolfson <todd@twolfson.com>
2019-06-17 04:13:53 -0700

    Added time delay as Electron has

--- child.js
+++ child.js
@@ -1,22 +1,24 @@
 let input = '';
 
-process.stdin.setEncoding('utf8');
+setTimeout(function () {
+  process.stdin.setEncoding('utf8');
 
-process.stdin.on('readable', function handleReadable () {
-  console.log('readable occurred');
-  // Use a loop to make sure we read all available data.
-  while (true) {
-    var stdinChunk = process.stdin.read();
-    console.log('chunk received', stdinChunk);
-    if (stdinChunk === null) {
-      break;
+  process.stdin.on('readable', function handleReadable () {
+    console.log('readable occurred');
+    // Use a loop to make sure we read all available data.
+    while (true) {
+      var stdinChunk = process.stdin.read();
+      console.log('chunk received', stdinChunk);
+      if (stdinChunk === null) {
+        break;
+      }
+      stdinContent += stdinChunk;
     }
-    stdinContent += stdinChunk;
-  }
-});
-process.stdin.on('end', function handleEnd () {
-  console.log('parsing json', stdinContent);
-  // var stdinOptions = JSON.parse(stdinContent);
-  // Object.assign(program, stdinOptions);
-  // launchBrowserWindow();
-});
+  });
+  process.stdin.on('end', function handleEnd () {
+    console.log('parsing json', stdinContent);
+    // var stdinOptions = JSON.parse(stdinContent);
+    // Object.assign(program, stdinOptions);
+    // launchBrowserWindow();
+  });
+}, 3000);
