a7238f49b559c3ef57751558590acb01750cc9ec
Todd Wolfson <todd@twolfson.com>
2019-06-17 05:02:25 -0700

    Found out the result is a wontfix in Electron that somehow didn't show up in earlier searches

--- parent.js
+++ parent.js
@@ -1,3 +1,7 @@
+// Turned out we were fucked from the start =/
+//   https://github.com/electron/electron/issues/4218
+// Need to move to a temporary file
+// Can pass it in as a parameter to load
 var electronPrebuilt = require('electron');
 var spawn = require('child_process').spawn;
 
