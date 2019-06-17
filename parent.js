18a8a906c5ebd71150236d5d9d897101b7ec10ee
Todd Wolfson <todd@twolfson.com>
2019-06-17 04:52:59 -0700

    Revert "Exploring detached flag"
    This reverts commit fe78e469ba81752737a72219f96e2e4cb3fc76a5.

--- parent.js
+++ parent.js
@@ -1,8 +1,6 @@
 var electronPrebuilt = require('electron');
 var spawn = require('child_process').spawn;
 
-var child = spawn(electronPrebuilt, ['child.js'], {
-  detached: true
-});
+var child = spawn(electronPrebuilt, ['child.js']);
 child.stdin.write('Hi');
 child.stdin.end();
