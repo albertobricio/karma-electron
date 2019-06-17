240a5ae603a29706648977115cad96d379d486ca
Todd Wolfson <todd@twolfson.com>
2019-06-17 04:25:29 -0700

    Moved to pipe/inherit/inherit

--- parent.js
+++ parent.js
@@ -1,7 +1,4 @@
 var spawn = require('child_process').spawn;
 
-var child = spawn('electron', ['child.js']);
-child.stdout.on('readable', () => {
-  while (chunk = child.stdout.read()) console.log(chunk.toString('utf8'));
-});
+var child = spawn('electron', ['child.js'], {stdio: ['pipe', 'inherit', 'inherit']});
 child.stdin.end('Hi');
