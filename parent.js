9b9a28dc43ef365e0bb4d7e2da108b4e5b103121
Todd Wolfson <todd@twolfson.com>
2019-06-17 04:10:36 -0700

    Added more complex method to isolate child/parent discrepancy

---
+++ parent.js
@@ -0,0 +1,7 @@
+var spawn = require('child_process').spawn;
+
+var child = spawn('node', ['child.js']);
+child.stdout.on('readable', () => {
+  while (chunk = child.stdout.read()) console.log(chunk.toString('utf8'));
+});
+child.stdin.end('Hi');
