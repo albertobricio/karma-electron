fa9060f51585081a549c6102c1ee3136bf872696
Todd Wolfson <todd@twolfson.com>
2019-06-17 04:19:08 -0700

    Moved process.stdin to top level

--- child.js
+++ child.js
@@ -8,24 +8,25 @@ app.on('window-all-closed', function handleWindowsClosed () {
   app.quit();
 });
 
-app.on('ready', function () {
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('readable', function handleReadable () {
-    console.log('readable occurred');
-    // Use a loop to make sure we read all available data.
-    while (true) {
-      var stdinChunk = process.stdin.read();
-      console.log('chunk received', stdinChunk);
-      if (stdinChunk === null) {
-        break;
-      }
-      stdinContent += stdinChunk;
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', function handleReadable () {
+  console.log('readable occurred');
+  // Use a loop to make sure we read all available data.
+  while (true) {
+    var stdinChunk = process.stdin.read();
+    console.log('chunk received', stdinChunk);
+    if (stdinChunk === null) {
+      break;
     }
-  });
-  process.stdin.on('end', function handleEnd () {
-    console.log('parsing json', stdinContent);
-    // var stdinOptions = JSON.parse(stdinContent);
-    // Object.assign(program, stdinOptions);
-    // launchBrowserWindow();
-  });
+    stdinContent += stdinChunk;
+  }
 });
+process.stdin.on('end', function handleEnd () {
+  console.log('parsing json', stdinContent);
+  // var stdinOptions = JSON.parse(stdinContent);
+  // Object.assign(program, stdinOptions);
+  // launchBrowserWindow();
+});
+
+// app.on('ready', function () {
+// });
