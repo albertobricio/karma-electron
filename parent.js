fe78e469ba81752737a72219f96e2e4cb3fc76a5
Todd Wolfson <todd@twolfson.com>
2019-06-17 04:52:07 -0700

    Exploring detached flag

--- parent.js
+++ parent.js
@@ -1,6 +1,8 @@
 var electronPrebuilt = require('electron');
 var spawn = require('child_process').spawn;
 
-var child = spawn(electronPrebuilt, ['child.js']);
+var child = spawn(electronPrebuilt, ['child.js'], {
+  detached: true
+});
 child.stdin.write('Hi');
 child.stdin.end();
