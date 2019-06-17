6e58df2f7a68902c0d0d5eaf73b5ac02f1fb76a6
Todd Wolfson <todd@twolfson.com>
2019-06-17 04:12:47 -0700

    Moved to direct usage of stream reading

--- child.js
+++ child.js
@@ -1,10 +1,22 @@
 let input = '';
 
-process.stdin.on('readable', () => {
-  let chunk;
-  while (chunk = process.stdin.read()) input += chunk;
-});
+process.stdin.setEncoding('utf8');
 
-process.stdin.on('end', () => {
-  console.log(input);
+process.stdin.on('readable', function handleReadable () {
+  console.log('readable occurred');
+  // Use a loop to make sure we read all available data.
+  while (true) {
+    var stdinChunk = process.stdin.read();
+    console.log('chunk received', stdinChunk);
+    if (stdinChunk === null) {
+      break;
+    }
+    stdinContent += stdinChunk;
+  }
+});
+process.stdin.on('end', function handleEnd () {
+  console.log('parsing json', stdinContent);
+  // var stdinOptions = JSON.parse(stdinContent);
+  // Object.assign(program, stdinOptions);
+  // launchBrowserWindow();
 });
